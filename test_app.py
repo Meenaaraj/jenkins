5a6b07f9ab039efa968c977ede4e5992b91565f3
Meenaaraj <meenarajk11@gmail.com>
2024-12-03 13:47:04 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,19 +1,6 @@
 # test_app.py
-import unittest
-from app import add_numbers, subtract_numbers
+import pytest
+from app import hello_world
 
-class TestCalculator(unittest.TestCase):
-    
-    def test_add_numbers(self):
-        self.assertEqual(add_numbers(3, 2), 5)
-        self.assertEqual(add_numbers(-1, 1), 0)
-        self.assertEqual(add_numbers(-1, -1), -2)
-    
-    def test_subtract_numbers(self):
-        self.assertEqual(subtract_numbers(5, 3), 2)
-        self.assertEqual(subtract_numbers(3, 3), 0)
-        self.assertEqual(subtract_numbers(-1, 1), -2)
-
-if __name__ == '__main__':
-    unittest.main()
-
+def test_hello_world():
+    assert hello_world() == "Hello, Jenkins!"
